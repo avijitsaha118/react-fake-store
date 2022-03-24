90b964264e6eb28df8986d11f755cfd02788a5c9
avijitsaha118 <sahaavijit908@gmail.com>
2022-03-24 16:03:40 +0600

    initial commit

---
+++ src/Component/AllProducts/AllProducts.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import SingleProduct from '../SingleProduct/SingleProduct';
+
+const AllProducts = (props) => {
+    const { setCartCount } = props;
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+            .then(res => res.json())
+            // .then(data => console.log(data));
+            .then(data => setProducts(data));
+    }, [])
+    console.log(products);
+    return (
+        <div>
+            <h1>All Products</h1>
+            <div className="row container">
+                {
+                    products.map((pd) => (<SingleProduct setCartCount={setCartCount} key={pd.id} product={pd}></SingleProduct>))
+                };
+            </div>
+        </div>
+    );
+};
+
+export default AllProducts;
